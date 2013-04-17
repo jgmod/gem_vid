147237ba8c32fd3ed36378ab39bf68ff2c049c3a
John Goh <jgmod@hotmail.com>
2013-04-17 11:40:56 -0400

    refactored js and view

--- app/assets/javascripts/youtube.js
+++ app/assets/javascripts/youtube.js
@@ -2,18 +2,7 @@
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://jashkenas.github.com/coffee-script/
 
-$("a[class='th radius']").click(function(){
-	var id = $(this).id;
-	console.log(id);
-	var iframe = $(this).data('iframe');
-	var flex_iframe = $("<a class='th radius'>").append(iframe).append("</a>");
-	$(this).replaceWith(flex_iframe);
-});
-$(".centered").each(function(){
-	$(this).mouseover(function(){
-		$(this).css("background","#87CEFA");
-	});
-	$(this).mouseout(function(){
-		$(this).css("background", "#FFF");
-	})
+$("div[class='large-6 columns']").click(function(){
+	var iframe = $(this).children().data("iframe");
+	$(this).replaceWith(iframe);
 });
